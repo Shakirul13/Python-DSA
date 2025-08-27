d81744e80fe208b74ffda79700ac68778cc72076
Md Shakirul Islam <ishakirul4@gmail.com>
2025-08-27 19:20:34 +0530

    Whether a number in both array

--- comparison.py
+++ comparison.py
@@ -1,14 +1,14 @@
 #Using dictionary,universal for any constraint of n
-#  n=[12,1,6,9,12,12,3,7,9,6,5,201]
+n=[12,1,6,9,12,12,3,7,9,6,5,201]
 m=[12,8,6,72,9,5]
-# freq={}
-# for num in n:
-#   freq[num]=freq.get(num,0)+1
-# res={}
-# for num in m:
-#   if num in freq:
-#     res[num]=freq[num]
-#print(res)
+freq={}
+for num in n:
+   freq[num]=freq.get(num,0)+1
+res={}
+for num in m:
+   if num in freq:
+     res[num]=freq[num]
+print(res)
 
 #if constraint: 1<=n[i]<=10,then number hashing is possible using list.But only for small length of list.
 # in that case:
