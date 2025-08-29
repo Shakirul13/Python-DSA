09cf75c164bad3a996d980075313c0d5d4870d89
Md Shakirul Islam <ishakirul4@gmail.com>
2025-08-29 16:29:44 +0530

    Implementing Dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -1,13 +1,13 @@
 n=[1,4,7,9,4,7,7,2,6]
 
-# hash_map=dict()
-# for i in n:
-#     if i in hash_map:
-#         hash_map[i]+=1
+hash_map=dict()
+for i in n:
+     if i in hash_map:
+         hash_map[i]+=1
 
-#     else:
-#         hash_map[i]=1
-# print(hash_map)
+     else:
+         hash_map[i]=1
+print(hash_map)
 
 #Using get() function:
 freq={}
