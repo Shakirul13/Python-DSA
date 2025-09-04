5829f1d01f0500caace435786151a354dde01e25
Md Shakirul Islam <ishakirul4@gmail.com>
2025-09-04 20:09:04 +0530

    Longest Consecutive Sequence

--- arr17.py
+++ arr17.py
@@ -2,8 +2,22 @@
 nums1=[1,99,101,98,2,5,3,100,1,1]
 nums2= [0,3,7,2,5,8,4,6,0,1]
 
-#Brute O(nlogn+n)~O(nlogn)
+#Brute Force
 def brute(nums):
+   n=len(nums)
+   max_count=0
+   for i in range(0,n):
+      num=nums[i]
+      count=1
+      while num+1 in nums:
+         count+=1
+         num=num+1
+      max_count=max(max_count,count)
+   return max_count
+print(brute(nums2))
+
+#Better O(nlogn+n)~O(nlogn)
+def better(nums):
  if not nums:
      return 0
  nums.sort()
@@ -17,7 +31,7 @@ def brute(nums):
         max_length=max(leng,max_length)
         leng=1
  return max(max_length,leng)
-print(brute(nums1))
+print(better(nums1))
 
 #Optimal Soulution O(n)
 def optimal(nums):
