cba0a267c2aa898baf2a83fcc14b1277ca40d7c0
Md Shakirul Islam <ishakirul4@gmail.com>
2025-09-07 19:00:29 +0530

    Set matrix zeroes

--- matrix0.py
+++ matrix0.py
@@ -24,3 +24,23 @@ def setZeroes(matrix):
                 matrix[i][j]=0
     return matrix
 print(setZeroes(matrix))
+
+matrix1=[[7,9,2,3],[20,8,0,10],[29,0,-10,5],[4,14,6,7]]
+
+#Optimal
+def optimal(matrix1):
+    r=len(matrix1)
+    c=len(matrix1[0])
+    row_track=[0 for _ in range(r)]
+    col_track=[0 for _ in range(c)]
+    for i in range(0,r):
+        for j in range(0,c):
+            if matrix1[i][j]==0:
+                row_track[i]=-1
+                col_track[j]=-1
+    for i in range (0,r):
+        for j in range(0,c):
+            if row_track[i]==-1 or col_track[j]==-1:
+                matrix1[i][j]=0
+    return matrix1
+print(optimal(matrix1))
