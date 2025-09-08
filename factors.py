87346f533813b1d0364b4b126152c5c00192167d
Md Shakirul Islam <ishakirul4@gmail.com>
2025-09-08 19:45:32 +0530

    Factors of a number

--- factors.py
+++ factors.py
@@ -1,15 +1,15 @@
 n=int(input("Enter the number:"))
 result=[]
 #Brute Force--> N
-# for i in range(1,n+1):
-#     if n%i==0:
-#         result.append(i)
+for i in range(1,n+1):
+    if n%i==0:
+         result.append(i)
 
 #Better --> N/2
-# for i in range (1,n//2):
-#     if n%i==0:
-#         result.append(i)
-# result.append(n)
+for i in range (1,n//2):
+   if n%i==0:
+      result.append(i)
+result.append(n)
 
 #Optimal Solution --> sqrt(N)+ NlogN (if sorting is done)
 from math import sqrt
