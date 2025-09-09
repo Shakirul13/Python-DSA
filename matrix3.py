9061462d09f32be8f76909e20ea97b244f8c2237
Md Shakirul Islam <ishakirul4@gmail.com>
2025-09-09 19:11:55 +0530

    1st commit

---
+++ matrix3.py
@@ -0,0 +1,11 @@
+matrix=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+
+#Brute force= TC - O(n^2);SC - O(n^2)
+def brute(matrix):
+    n=len(matrix)
+    result=[[0 for _ in range(n)]for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+           result[j][n-1-i]=matrix[i][j]
+    return result
+print(brute(matrix))
