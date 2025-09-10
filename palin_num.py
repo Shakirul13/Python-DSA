a26d41e2e0610d5278d6dfce42237c9af69f24ab
Md Shakirul Islam <ishakirul4@gmail.com>
2025-09-10 20:05:52 +0530

    Palindrome Number

--- palin_num.py
+++ palin_num.py
@@ -1,12 +1,15 @@
-n=int(input("Enter the number:"))
-new=n
-rev=0
-while n>0:
+n1=121
+n2=140
+def palindrome(n):
+   new=n
+   rev=0
+   while n>0:
     digit=n%10
     n=n//10
     rev=rev*10 + digit
-if rev==new:
-    print("palindrome")
-else:
-    print("Not palindrome")
-    
+   if rev==new:
+    return True
+   else:
+    return False
+print(palindrome(n1))
+print(palindrome(n2))
