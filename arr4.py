6c6b99b465be848005054bad44d2e7b399e45543
Md Shakirul Islam <ishakirul4@gmail.com>
2025-09-11 19:25:30 +0530

    Remove duplicates from sorted array

--- arr4.py
+++ arr4.py
@@ -1,11 +1,14 @@
 nums=[1,1,1,2,3,4,4,7,9,9,9,10]
-n= len(nums)
-i=0
-j=i+1
-while j <n :
-    if nums[i]!=nums[j]:
-        i+=1
-        nums[i],nums[j]=nums[j],nums[i]
-    j+=1
-print(nums)
-print(f"Size of new list is :{i+1}")
+#Unique elements in a sorted array
+def duplicates(nums):
+    n= len(nums)
+    i=0
+    j=i+1
+    while j <n :
+         if nums[i]!=nums[j]:
+              i+=1
+              nums[i],nums[j]=nums[j],nums[i]
+         j+=1
+    print(f"Size of new list is :{i+1}")
+    return nums
+print(f"new list is :{duplicates(nums)}")
