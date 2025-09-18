c45c094b8ac9e9581c413f4337852467843b756b
Md Shakirul Islam <ishakirul4@gmail.com>
2025-09-18 19:49:22 +0530

    Binary Search

---
+++ binarysearch.py
@@ -0,0 +1,16 @@
+nums=[2,5,8,12,19,46,131]
+target=5
+def binarysearch(nums,target):
+    n=len(nums)
+    low=0
+    high=n-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]==target:
+            return mid
+        elif nums[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+print(binarysearch(nums,target))
