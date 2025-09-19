351a8b150b7c954eed0767d896229d4d9536077e
Md Shakirul Islam <ishakirul4@gmail.com>
2025-09-19 18:41:45 +0530

    Binary Search

--- binarysearch.py
+++ binarysearch.py
@@ -1,5 +1,6 @@
 nums=[2,5,8,12,19,46,131]
-target=5
+target=131
+#Iterative Approach
 def binarysearch(nums,target):
     n=len(nums)
     low=0
@@ -14,3 +15,16 @@ def binarysearch(nums,target):
             high=mid-1
     return -1
 print(binarysearch(nums,target))
+
+#Recursive Approach
+def bs(nums,low,high,target):
+    if low>high:
+        return -1
+    mid=(low+high)//2
+    if nums[mid]<target:
+        return bs(nums,mid+1,high,target)
+    elif nums[mid]==target:
+        return mid
+    else:
+        return bs(nums,low,mid-1,target)
+print(bs(nums,0,len(nums)-1,target))
