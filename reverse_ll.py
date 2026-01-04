73db3347bcbd153dc9e1a1b98a04837898f7ec55
Md Shakirul Islam <ishakirul4@gmail.com>
2026-01-04 21:18:22 +0530

    Reverse a ll

---
+++ reverse_ll.py
@@ -0,0 +1,21 @@
+#Reverse a linked list:logic
+#Brute force solution : reverse the ll by changing the value
+#5->7->10->2->80
+temp=head
+stack=[]
+while temp is not None:
+    stack.append(temp.val)
+    temp=temp.next
+temp=head
+while temp is not None:
+    e=stack.pop()
+    temp.val=e
+    temp=temp.next
+#optimal
+temp=head
+prev=None
+while temp is not None:
+    front=temp.next
+    temp.next=prev
+    prev=temp
+    temp=front
